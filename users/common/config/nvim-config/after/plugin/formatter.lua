108ad6006406f7265e33760ebe32ef9db37501ad
Aditya Gaitonde <gaitondeaditya@gmail.com>
2022-12-26 10:05:43 +0530

    fix: formatter

--- users/common/config/nvim-config/after/plugin/formatter.lua
+++ users/common/config/nvim-config/after/plugin/formatter.lua
@@ -4,7 +4,7 @@ require("formatter").setup({
 	log_level = vim.log.levels.DEBUG,
 	filetype = {
 		lua = { require("formatter.filetypes.lua").stylua },
-		nix = { require("formatter.filetypes.nix").nixpkgs_fmt },
+		-- nix = { require("formatter.filetypes.nix").nixpkgs_fmt },
 		javascript = {},
 		["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
 	},
