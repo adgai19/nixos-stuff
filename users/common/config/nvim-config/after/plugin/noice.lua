d4debd510d7fcef3ab666abc5d3ca9f0baf61e82
Aditya Gaitonde <gaitondeaditya@gmail.com>
2023-01-12 12:39:25 +0530

    disabling noice till noice+nightly sorts out

--- users/common/config/nvim-config/after/plugin/noice.lua
+++ users/common/config/nvim-config/after/plugin/noice.lua
@@ -1,16 +1,16 @@
-require("noice").setup({
-	presets = {
-		inc_rename = true,
-		command_palette = true,
-	},
-	routes = {
-		{
-			filter = {
-				event = "msg_show",
-				kind = "",
-				find = "written",
-			},
-			opts = { skip = true },
-		},
-	},
-})
+-- require("noice").setup({
+-- 	presets = {
+-- 		inc_rename = true,
+-- 		command_palette = true,
+-- 	},
+-- 	routes = {
+-- 		{
+-- 			filter = {
+-- 				event = "msg_show",
+-- 				kind = "",
+-- 				find = "written",
+-- 			},
+-- 			opts = { skip = true },
+-- 		},
+-- 	},
+-- })
