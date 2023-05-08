4c34a66e2cd931ad6d6c345dec486604cc0ef3b0
Aditya Gaitonde <gaitondeaditya@gmail.com>
2023-05-08 12:24:09 +0530

    feat(git): new log format

--- hosts/legion/modules/git.nix
+++ hosts/legion/modules/git.nix
@@ -23,6 +23,8 @@ in
       aliases = {
         ds = "diff --staged";
         ap = "add -p";
+        lgf = "log --graph --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%an%C(reset)%C(bold yellow)%d%C(reset) %C(dim white)- %s%C(reset)' --all";
+        slog="log --graph --all --topo-order --pretty='format:%h %ai %s%d (%an)'";
       };
       extraConfig = {
         init = {
