47c323ddacbf1bc90c627d640920e37679db479e
Aditya Gaitonde <gaitondeaditya@gmail.com>
2023-10-29 10:38:46 +0530

    fix(flake.nix): fix poetry overlay

--- flake.nix
+++ flake.nix
@@ -163,7 +163,7 @@
 
       overlays = [
         # inputs.neovim-nightly.overlay
-        inputs.poetry2nix.overlay
+        inputs.poetry2nix.overlays.default
         inputs.firefox-overlay.overlay
         self.overlays.default
       ];
