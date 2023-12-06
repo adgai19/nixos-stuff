689e65599672fa66f0dc2f50159efb287ed740bc
Aditya Gaitonde <gaitondeaditya@gmail.com>
2023-12-06 14:24:29 +0530

    fix(kafka.nix): working kafka flake

--- shell-flakes/kafka-flake.nix
+++ shell-flakes/kafka-flake.nix
@@ -6,31 +6,56 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    devshell.url = "github:numtide/devshell";
   };
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
       imports = [
         inputs.process-compose-flake.flakeModule
+        inputs.devshell.flakeModule
       ];
       perSystem = { self', pkgs, lib, ... }: {
         process-compose.default = {
-          settings.environment.FOOBAR = "demo";
+          settings.log_location = "/tmp/shell.log";
+          settings.log_level = "debug";
           settings.processes = {
             kafka = {
-              command = "${pkgs.apacheKafka}/bin/kafka-server-start.sh ${pkgs.apacheKafka}/config/server.properties" ;
+              command = "${pkgs.apacheKafka}/bin/kafka-server-start.sh ${pkgs.apacheKafka}/config/server.properties";
               # environment = { KAFKA_HEAP_OPTS = "-Xmx256M -Xms128M"; };
-              # dependencies = [ "zookeeper" ];
             };
             zookeeper = {
-              command = "${pkgs.zookeeper}/bin/zkServer.sh ${pkgs.apacheKafka}/config/zookeeper.properties";
+              command = " ${pkgs.zookeeper}/bin/zkServer.sh start  /home/aditya/code/kafka/conf/zoo.cfg";
               # args = [ "start-foreground" ];
-              # environment = { ZOO_LOG_DIR = "/tmp/zookeeper"; };
+              environment = {
+                ZOO_LOG_DIR = "/tmp/zookeeper";
+                ZOO_DATADIR = "/tmp/zookeeper";
+                ZOOCFGDIR = "/home/aditya/code/kafka/conf";
+              };
             };
           };
+        };
+        devshells.default = {
+          env = [
+          ];
+          commands = [
+            {
+              help = "print hello";
+              name = "hello";
+              command = "echo hello";
+            }
+            {
+
+              help = "test";
+              name = "topics";
+              command = "${pkgs.apacheKafka}/bin/kafka-topics.sh --bootstrap-server=localhost:9092 --list";
+            }
+          ];
+          packages = [
+          ];
         };
 
       };
     };
 
-}
+}   
