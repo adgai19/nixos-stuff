76707cdafb652f37bc14f46f0bb91722a71b6a61
Aditya Gaitonde <gaitondeaditya@gmail.com>
2023-12-11 10:23:43 +0530

    Legion (#6)
    * bump: version 1.1.0 → 1.2.0
    
    * docs(changelog): update changelog
    
    * feat(git): set up autoSetupRemote
    
    * docs(Readme): change readme
    
    * feat(neovim): remove null-ls
    
    * build(flake.lock): update dep
    
    * build(flake.lock): update dep
    
    * fix(go): change to go 1.21
    
    * feat(programs/legion): add doppler
    
    * build(flake.lock): update dep
    
    * build(flake.lock): update dep
    
    * kernel boot issue
    
    This reverts commit 8f7c1d352a9d4b6f4068a62218af280486b59835.
    
    * build(flake.lock): update dep
    
    * feat(neovim): add python setup
    
    * build(flake.lock): update dep
    
    * build(flake.lock): update dep
    
    * feat(libvertd): libvertd setup
    
    * feat(legion): reinstall virtualbox
    
    * build(flake.lock): update dep
    
    * build(flake.lock): update dep
    
    * build(flake.lock): update dep
    
    * fix(neovim): fix yamlls
    
    * feat: added kafka and diffview-nvim
    
    * fix(flake.nix): fix poetry overlay
    
    * build(flake.lock): update dep
    
    * fix(configuration.nix): fix firefox and nvidia driver sources
    
    * build(flake.lock): update dep
    
    * fix(firefox): swap firefox overlay
    
    * feat(wezterm): change font to Maple Mono
    
    * build(flake.lock): update dep
    
    * build(flake.lock): update dep
    
    * build(flake.lock): update dep
    
    * build(flake.lock): update dep
    
    * feat(i3): add bumblebee status
    
    * build(flake.lock): update dep
    
    * feat(*): upgrade everything for new laptop
    
    * docs(changelog): update changelog
    
    * build(flake.lock): update dep
    
    * fix(*): system fixes
    
    * bump: version 1.2.0 → 1.3.0
    
    * feat(i3): added config
    
    * feat(neovim): moving to nightly and fixing treesitter context commentstring
    
    * feat(neovim): rust toolchatin
    
    * build(flake.lock): update dep
    
    * feat(shell-flakes): kafka flake init
    
    * fix(kafka.nix): working kafka flake (#5)
    
    Co-authored-by: Aditya Gaitonde <gaitondeaditya@gmail.com>
    
    * build(flake.lock): update dep
    
    * build(flake.lock): update dep
    
    * fix(neovim): disable harpoon
    
    * fix(neovim): fix go.nvim
    
    ---------
    
    Co-authored-by: agaitonde-sp <152586693+agaitonde-sp@users.noreply.github.com>

--- users/common/config/nvim-config/lua/adgai/keymaps/harpoon.lua
+++ users/common/config/nvim-config/lua/adgai/keymaps/harpoon.lua
@@ -1,34 +1,34 @@
 local nnoremap = require("adgai.keymaps.helpers").nnoremap
 
-nnoremap("<leader>na", require("harpoon.mark").add_file)
-nnoremap("<leader>ni", require("harpoon.ui").toggle_quick_menu)
-nnoremap("<leader>np", require("harpoon.cmd-ui").toggle_quick_menu)
-
-nnoremap("<M-t>", '<cmd>lua require("harpoon.ui").nav_file(1)<cr>')
-
-nnoremap("<M-n>", '<cmd>lua require("harpoon.ui").nav_file(2)<cr>')
-nnoremap("<M-h>", '<cmd>lua require("harpoon.ui").nav_file(3)<cr>')
-nnoremap("<M-e>", '<cmd>lua require("harpoon.ui").nav_file(4)<cr>')
-
-nnoremap("<M-l>", require("harpoon.ui").toggle_quick_menu)
-nnoremap("<M-c>", require("harpoon.ui").nav_file)
-
-nnoremap(
-	"<M-w>",
-	'<cmd>lua require("harpoon.term").sendCommand(1, 1)<CR>:lua require("harpoon.term").gotoTerminal(1)<CR>a<CR>'
-)
-nnoremap(
-	"<M-r>",
-	'<cmd>lua require("harpoon.term").sendCommand(2, 2)<CR>:lua require("harpoon.term").gotoTerminal(4)<CR>a<CR>'
-)
-nnoremap(
-	"<M-f>",
-	'<cmd>lua require("harpoon.term").sendCommand(1, 3)<CR>:lua require("harpoon.term").gotoTerminal(4)<CR>a<CR>'
-)
-nnoremap(
-	"<M-u>",
-	'<cmd>lua require("harpoon.term").sendCommand(2, 4)<CR>:lua require("harpoon.term").gotoTerminal(4)<CR>a<CR>'
-)
-
-nnoremap("<M-,>", '<cmd>lua require("harpoon.term").gotoTerminal(1)<CR>a<CR>')
-nnoremap("<M-m>", '<cmd>lua require("harpoon.term").gotoTerminal(2)<CR>a<CR>')
+-- nnoremap("<leader>na", require("harpoon.mark").add_file)
+-- nnoremap("<leader>ni", require("harpoon.ui").toggle_quick_menu)
+-- nnoremap("<leader>np", require("harpoon.cmd-ui").toggle_quick_menu)
+--
+-- nnoremap("<M-t>", '<cmd>lua require("harpoon.ui").nav_file(1)<cr>')
+--
+-- nnoremap("<M-n>", '<cmd>lua require("harpoon.ui").nav_file(2)<cr>')
+-- nnoremap("<M-h>", '<cmd>lua require("harpoon.ui").nav_file(3)<cr>')
+-- nnoremap("<M-e>", '<cmd>lua require("harpoon.ui").nav_file(4)<cr>')
+--
+-- nnoremap("<M-l>", require("harpoon.ui").toggle_quick_menu)
+-- nnoremap("<M-c>", require("harpoon.ui").nav_file)
+--
+-- nnoremap(
+-- 	"<M-w>",
+-- 	'<cmd>lua require("harpoon.term").sendCommand(1, 1)<CR>:lua require("harpoon.term").gotoTerminal(1)<CR>a<CR>'
+-- )
+-- nnoremap(
+-- 	"<M-r>",
+-- 	'<cmd>lua require("harpoon.term").sendCommand(2, 2)<CR>:lua require("harpoon.term").gotoTerminal(4)<CR>a<CR>'
+-- )
+-- nnoremap(
+-- 	"<M-f>",
+-- 	'<cmd>lua require("harpoon.term").sendCommand(1, 3)<CR>:lua require("harpoon.term").gotoTerminal(4)<CR>a<CR>'
+-- )
+-- nnoremap(
+-- 	"<M-u>",
+-- 	'<cmd>lua require("harpoon.term").sendCommand(2, 4)<CR>:lua require("harpoon.term").gotoTerminal(4)<CR>a<CR>'
+-- )
+--
+-- nnoremap("<M-,>", '<cmd>lua require("harpoon.term").gotoTerminal(1)<CR>a<CR>')
+-- nnoremap("<M-m>", '<cmd>lua require("harpoon.term").gotoTerminal(2)<CR>a<CR>')
