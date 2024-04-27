1320d75bf954b55046814d92834f7f5c9ff181db
Aditya Gaitonde <gaitondeaditya@gmail.com>
2024-04-27 16:58:19 +0530

    fix(zsh): change atuin search mode

--- users/common/modules/zsh.nix
+++ users/common/modules/zsh.nix
@@ -98,7 +98,7 @@ in
         auto_sync = true;
         sync_frequency = "5m";
         sync_address = "https://api.atuin.sh";
-        search_mode = "prefix";
+        search_mode = "fuzzy";
       };
     };
   };
