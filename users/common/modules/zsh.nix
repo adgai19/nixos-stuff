150926022febee874dfb9167dae7f4d978040fb4
Aditya Gaitonde <gaitondeaditya@gmail.com>
2025-05-06 23:00:16 +0530

    build(flake.lock): update dep

--- users/common/modules/zsh.nix
+++ users/common/modules/zsh.nix
@@ -44,7 +44,7 @@ in
         setopt HIST_IGNORE_SPACE
         setopt HIST_SAVE_NO_DUPS
         unsetopt HIST_VERIFY
-        export DIRENV_LOG_FORMAT=
+        export DIRENV_LOG_FORMAT=""
         # bindkey "^P" up-line-or-search
         # bindkey "^N" down-line-or-search
         bindkey -s "^o" "tmux-sessionizer^M"
