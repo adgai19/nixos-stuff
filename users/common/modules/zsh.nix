9cd231e8bce29e98bf2c57ec333dcadfb06aa125
Aditya Gaitonde <gaitondeaditya@gmail.com>
2025-10-27 14:26:43 +0530

    build(flake.lock): update dep

--- users/common/modules/zsh.nix
+++ users/common/modules/zsh.nix
@@ -23,6 +23,9 @@ in
         agedec="age -d -i  ~/.age/identities/identities.txt";
         ageenc="age -r age1yubikey1q2xqk42vclfcvuwwcslu2z2jsk6mkd5kf6l2ms8cfgmcfpjc3lrfj24zj7x -r age1yubikey1q22t4dl57x24eqqvnja6um5uc5qe7fgf07s2xtg2g6a8lcs3l7jh6w3uy0d -r age1se1qdlg2e3pgsu2dnw247qezznhqapat65sp5cy0h997qtvlu8zhkfy5k6u2aj -o";
         getsec="openssl rand -base64 24 | tr -dc 'A-Za-z0-9' | head -c 24 | pbcopy";
+	miUp="migrate -database $DB_URL -path database/migrations -verbose up";
+	mic="migrate create -ext sql -dir database/migrations -seq"
+
       };
       defaultKeymap = "emacs";
       history = {
