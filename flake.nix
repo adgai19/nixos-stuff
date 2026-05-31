f5bc89d304ba11b96d3b0feab3f94e5c3a236ac6
Aditya Gaitonde <gaitondeaditya@gmail.com>
2026-05-31 16:57:06 +0530

    build(flake.lock): update dep

--- flake.nix
+++ flake.nix
@@ -56,21 +56,21 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ home-manager, neovim-nightly, nixpkgs, nixpkgs-unstable, nixpkgs-unstable-small, self, sops-nix,  stylix, nixpkgs-stable,  nix-darwin, ... }:
+  outputs = inputs@{ home-manager, neovim-nightly, nixpkgs, nixpkgs-unstable, nixpkgs-unstable-small, self, sops-nix, stylix, nixpkgs-stable, nix-darwin, ... }:
     let
       system = "x86_64-linux";
-        linuxSystem = "x86_64-linux";
-  macSystem = "aarch64-darwin";
+      linuxSystem = "x86_64-linux";
+      macSystem = "aarch64-darwin";
 
-   pkgsLinux = import nixpkgs {
-    system = linuxSystem;
-    config = { allowUnfree = true; };
-  };
+      pkgsLinux = import nixpkgs {
+        system = linuxSystem;
+        config = { allowUnfree = true; };
+      };
 
-  pkgsMac = import nixpkgs {
-    system = macSystem;
-    config = { allowUnfree = true; };
-  };
+      pkgsMac = import nixpkgs {
+        system = macSystem;
+        config = { allowUnfree = true; };
+      };
 
       pkgs = import nixpkgs {
         inherit linuxSystem;
@@ -91,7 +91,7 @@
         config = { allowUnfree = true; };
       };
 
-     pkgs-mac = import nixpkgs {
+      pkgs-mac = import nixpkgs {
         inherit macSystem;
         config = { allowUnfree = true; };
       };
@@ -191,22 +191,38 @@
 
 
       darwinConfigurations = {
-        Adityas-MacBook-Pro =nix-darwin.lib.darwinSystem {
-         modules = [
-         ./system/darwin/configuration.nix 
+        Adityas-MacBook-Pro = nix-darwin.lib.darwinSystem {
+          modules = [
+            ./system/darwin/configuration.nix
 
- {
-            }
-             home-manager.darwinModules.home-manager
-             {
+            { }
+            home-manager.darwinModules.home-manager
+            {
+              nixpkgs.overlays = [
+                (final: prev: {
+                  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+                    (python-final: python-prev: {
+                      a2a-sdk = python-prev.a2a-sdk.overridePythonAttrs (old: {
+                        disabledTests = (old.disabledTests or [ ]) ++ [
+                          "test_notification_triggering_with_in_message_config_e2e"
+                          "test_notification_triggering_after_config_change_e2e"
+                          "test_trace_function_sync_attribute_extractor_error_logged"
+                        ];
+                      });
+                    })
+                  ];
+                })
+              ];
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit  inputs pkgs-unstable-mac pkgs-stable-mac; 
-              system = "aarch64-darwin"; };
-              home-manager.users.adgai = import ./hosts/mac/home.nix ;
+              home-manager.extraSpecialArgs = {
+                inherit inputs pkgs-unstable-mac pkgs-stable-mac;
+                system = "aarch64-darwin";
+              };
+              home-manager.users.adgai = import ./hosts/mac/home.nix;
             }
-         ];
-      };
+          ];
+        };
       };
     };
 }
